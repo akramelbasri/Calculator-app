7dedfae2e66fb26bebb01d3e9f9a0e4c2620783a
akram <elbasriakram771@gmail.com>
2022-11-01 22:05:42 +0100

    work is done

--- calcule.js
+++ calcule.js
@@ -1,58 +1,43 @@
+// gets elements
 let container = document.getElementById("container");
 let btns = document.getElementsByTagName("button");
 let input = document.getElementById("ecran");
-//------ fun number 1 -------------
+let head = document.getElementById("header");
+let foot = document.getElementById("footer");
+
+//------ fun number 1 -------------1b1919
 function modeBlack() {	
+	head.style.color = "white" ;
+	footer.style.color = "white" ;	
+	head.style.backgroundColor = "black" ;
+	footer.style.backgroundColor = "black" ;
 	container.style.backgroundColor = "black" ;
 	for (var i = btns.length - 1; i >= 0; i--) {
 		btns[i].style.backgroundColor = "#83FA44" ;
 	}
 	input.style.backgroundColor = "#F2F5F5" ; 
 }
-//------fin fun number1-----------
-
 // fun 2--------------------
 function modeNormal() {
+	head.style.color = "black" ;
+	footer.style.color = "black" ;	
+	head.style.backgroundColor = "#D4FFFF" ;
+	footer.style.backgroundColor = "#D4FFFF" ;
 	container.style.backgroundColor = "white" ;
 	for (var i = btns.length - 1; i >= 0; i--) {
 		btns[i].style.backgroundColor = "#F2F5F5" ;
 	}
 	input.style.backgroundColor = "#F1F8F7" ; 
 }
-
-
 function ajoute(e) {
-	//ajoute des valeurs au inpt1
 	let nmbr = e.textContent ;
 	input.value += nmbr ; 
 }
-
 function DropNombres(){
 	input.value ="";
 }
-
 //------ methode evel pour calculer -----
-
 function calcule(){
 	var result = eval(input.value);
 	input.value = result ;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
